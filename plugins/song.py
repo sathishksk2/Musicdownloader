e096211abc76aa7a8b90187abd01e285bf3b6348
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-05-24 14:08:32 +0530

    Update song.py
    😐

--- plugins/song.py
+++ plugins/song.py
@@ -83,7 +83,7 @@ async def song(client, message):
             ydl.process_info(info_dict)
 #        artist = str(info_dict["artist"])
 #        uploader = str(info_dict["uploader"])
-        ironman = f'• **Tittle** : __{title}__\n• **Channel** : `{thor}`\n• **Link** : {link}\n• **Requested For** : `{query}`'
+#       ironman = f'• **Tittle** : __{title}__\n• **Channel** : `{thor}`\n• **Link** : {link}\n• **Requested For** : `{query}`'
         rep = f"🎧 𝗧𝗶𝘁𝘁𝗹𝗲 : [{title[:35]}]({link})\n⏳ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻 : `{duration}`\n👀 𝗩𝗶𝗲𝘄𝘀 : `{views}`\n\n📮 **By** : [{message.from_user.first_name}](tg://user?id={message.from_user.id})"
         buttons = InlineKeyboardMarkup([[InlineKeyboardButton('sᴇᴀʀᴄʜ ɪɴʟɪɴᴇ', switch_inline_query_current_chat=f'')]])
         secmul, dur, dur_arr = 1, 0, duration.split(':')
