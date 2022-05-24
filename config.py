cbc3965e962ad0f65890057753074d89c4cf6188
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-05-24 12:19:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import re
 from os import getenv
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
@@ -9,3 +10,6 @@ AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "Avengers")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+START_MSG = os.environ.get("START_MSG", "<b>Hello {} 👋,\nI am a Music Downloader 😎\nI can Download songs from Spotify,JioSaavn, YouTube etc..\nCheck /help for more info</b>")
+OWNER = os.environ.get("OWNER", "Peter Parker") 
+msg = {}
