35ae687b386853e1ecf319abebfb926d3112716b
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-05-24 12:26:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,19 @@ from pyrogram import Client
 import config
 import os
 
+BOT_TOKEN = config.BOT_TOKEN
+API_HASH = config.API_HASH
+API_ID = config.API_ID
+
 if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
     bot = Client(
         "Music-Downloader",
-        bot_token=config.BOT_TOKEN,
-        api_hash=config.API_HASH,
-        api_id=config.API_ID,
+        bot_token=BOT_TOKEN,
+        api_hash=API_HASH,
+        api_id=API_ID,
         plugins=plugins
     )
     bot.run()
