6176a917cd12a19b8055a9e2ba9c4e60b25c0da3
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-05-25 12:07:55 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -92,7 +92,7 @@ async def song(client, message):
 #       await client.send_chat_action(chat_id, "upload_photo")
 #       await message.reply_photo(thumbnail, caption=ironman, parse_mode='md', ttl_seconds=500)
 #       await client.send_chat_action(chat_id, "upload_audio")
-        await message.reply_audio(audio=audio_file, caption=reply, parse_mode='md', quote=True, title=title, duration=dur, performer=str(info_dict["uploader"]), reply_markup=buttons, thumb=moonknight)
+        await message.reply_audio(audio=audio_file, caption=reply, parse_mode='md', quote=True, title=title, duration=dur, performer=str(info_dict["uploader"]), reply_markup=buttons)
         await m.delete()
     except Exception as e:
         await m.edit(f'😔**Failed**\n\n__Report this Error to my [Master](https://t.me/{config.OWNER})\nOr try__ : `/spotify {query}`')
