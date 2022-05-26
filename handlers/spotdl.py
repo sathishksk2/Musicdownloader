828795fa3fe10db679e63c4c06b8a1e7a3c23e94
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-05-26 11:13:11 +0530

    Update spotdl.py

--- handlers/spotdl.py
+++ handlers/spotdl.py
@@ -11,8 +11,7 @@ async def download_from_spotify(download_path: str, link: List[str]):
     os.system(f'spotdl {link}')
     os.chdir("..")
 
-async def send_songs_from_directory(
-    directory_path: str, client, message: Message):
+async def send_songs_from_directory(directory_path: str, client, message: Message):
     directory = os.listdir(directory_path)
     chat_id = message.chat.id
 #   Title = os.basename(directory_path)
@@ -24,6 +23,6 @@ async def send_songs_from_directory(
             await client.send_chat_action(chat_id, "upload_audio")
             await client.send_audio(chat_id,caption=caption,audio=open(f'{directory_path}/{file}', 'rb'))
         except Exception:
-            client.send_message(chat_id, text=f"Failed to send song {file}")
+           await client.send_message(chat_id, text=f"Failed to send song {file}")
 
     subprocess.run(['rm', '-r', directory_path])
