186054607a7da0e20254c27a69d2ce7ed904efb4
sathishksk2 <105577023+sathishksk2@users.noreply.github.com>
2022-06-20 14:29:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 import re
 from os import getenv
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5411372727:AAH-z8UwR_G2r77CI2PVqovNreDosUEkWXo")
+API_ID = int(os.environ.get("API_ID", "1677067"))
+API_HASH = os.environ.get("API_HASH", "0d8c8aabe36b01db6a26a7f2780fa660")
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "Avengers")
